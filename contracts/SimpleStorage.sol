f5ffcf07b7507ef503918d779200bc7b1b0ea458
AbhishekMauryaGEEK <abhiotaku.am01@gmail.com>
2026-03-02 18:01:50 +0530

    'change'

---
+++ contracts/SimpleStorage.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.29;
+
+contract SimpleStorage {
+    //boolean,uint,int,address,bytes
+    // bool hasFavouriteNumber = false;
+    // uint256 favoriteNumber = 123;
+    // string favoriteNumber1 = "Five";
+    // int256 favoriteNumber2 = 56;
+    // address myaddress = 0x0014d3560721fc65008d6a761c259037D5fEcA73 ;
+    // bytes32 favoriteBytes = "cat";
+    unit256    
+}
